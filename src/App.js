8fb6ed07ace1c060a914efd416084b75659ea1c2
tosico85 <tosico85@gmail.com>
2022-02-08 23:39:02 +0900

    designed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Board from "./Board";
 import React, { useState } from "react";
-import "./assets/scss/styles.scss";
 
 function App() {
-  const [curIdx, setCurIdx] = useState(0);
+  const [curIdxHyeon, setCurIdxHyeon] = useState(0);
+  const [curIdxHun, setCurIdxHun] = useState(0);
 
   const quizes_hyeon = [
     { id: 0, quiz: "1 + 1은?", answer: "2" },
     { id: 1, quiz: "4는 ㅁ보다 1 작다.", answer: "5" },
+    { id: 2, quiz: "7는 ㅁ보다 1 크다.", answer: "6" },
     { id: 9999, quiz: "축하합니다. 문제를 모두 풀었습니다.", answer: "" },
   ];
 
@@ -17,29 +18,39 @@ function App() {
     { id: 9999, quiz: "축하합니다. 문제를 모두 풀었습니다.", answer: "" },
   ];
 
-  const onPass = () => {
-    if (curIdx < quizes_hyeon.length - 1) {
-      setCurIdx((preIdx) => preIdx + 1);
+  const onPass = (who) => {
+    if (who === "hun") {
+      if (curIdxHun < quizes_hun.length - 1) {
+        setCurIdxHun((preIdx) => preIdx + 1);
+      }
+    } else {
+      if (curIdxHyeon < quizes_hyeon.length - 1) {
+        setCurIdxHyeon((preIdx) => preIdx + 1);
+      }
     }
   };
 
   return (
     <div className="main">
       <div className="board">
-        <h1 className="title">훈이문제</h1>
+        <h1 className="title">훈이 문제</h1>
         {quizes_hun.map((item, index) => {
-          if (index === curIdx) {
-            return <Board quiz={item} onPass={onPass} key={item.id} />;
+          if (index === curIdxHun) {
+            return (
+              <Board who={"hun"} quiz={item} onPass={onPass} key={item.id} />
+            );
           } else {
             return null;
           }
         })}
       </div>
       <div className="board">
-        <h1 className="title">현이문제</h1>
+        <h1 className="title">현이 문제</h1>
         {quizes_hyeon.map((item, index) => {
-          if (index === curIdx) {
-            return <Board quiz={item} onPass={onPass} key={item.id} />;
+          if (index === curIdxHyeon) {
+            return (
+              <Board who={"hyeon"} quiz={item} onPass={onPass} key={item.id} />
+            );
           } else {
             return null;
           }
--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import "./assets/scss/pages/board.scss";
 
-function Board({ quiz, onPass }) {
+function Board({ who, quiz, onPass }) {
   const [answer, setAnswer] = useState("");
   const [isCorrect, setIsCorrect] = useState(false);
   const [resultMsg, setResultMsg] = useState("");
@@ -9,6 +8,8 @@ function Board({ quiz, onPass }) {
 
   const onSubmit = (event) => {
     event.preventDefault();
+    if (answer === "") return;
+
     if (answer === quiz.answer) {
       setResultMsg("정답입니다!");
       setResultCss("result-blue");
@@ -18,25 +19,30 @@ function Board({ quiz, onPass }) {
       setResultMsg("땡! 틀렸습니다.");
       setResultCss("result-red");
     }
+    setAnswer("");
   };
 
   const onChange = (event) => setAnswer(event.target.value);
 
   return (
-    <div>
+    <div className="board-content">
       <form onSubmit={onSubmit}>
         <h1>{quiz.quiz}</h1>
         {quiz.id !== 9999 && (
           <div>
-            <input type="text" onChange={onChange} />
-            <button>제출</button>
-            <h2 className={resultCss}>{resultMsg}</h2>
+            <div className="reg-box">
+              <input className="input-reg" type="text" onChange={onChange} />
+              <button className="button-reg">제출</button>
+            </div>
+            <h2 className={"result " + resultCss}>{resultMsg}</h2>
           </div>
         )}
         {isCorrect && (
-          <button className="next-button" onClick={onPass}>
-            다음
-          </button>
+          <div className="next-button-area">
+            <button className="next-button" onClick={() => onPass(who)}>
+              다음 문제
+            </button>
+          </div>
         )}
       </form>
     </div>
